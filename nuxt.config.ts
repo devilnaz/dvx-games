c71121fc9a1b736db565e909b426db02cfd4dbd2
Alex W0lf <w0lfram@yandex.ru>
2024-05-24 02:06:39 +0300

    Terms redirect added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,41 +1,62 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
-  devtools: { enabled: true },
-  css: ['~/assets/css/main.scss'],
-  components: [
-    {
-      path: '~/components',
-      pathPrefix: false,
-    },
-  ],
-  alias: {
-    '@components': './components',
-    '@assets': './assets',
-    '@stores': './stores',
-  },
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: `
+   ssr: false,
+   devtools: { enabled: true },
+   css: ['~/assets/css/main.scss'],
+   components: [
+      {
+         path: '~/components',
+         pathPrefix: false,
+      },
+   ],
+   alias: {
+      '@components': './components',
+      '@assets': './assets',
+      '@stores': './stores',
+   },
+   vite: {
+      css: {
+         preprocessorOptions: {
+            scss: {
+               additionalData: `
                 @import "./assets/css/_mixins.scss";
               `,
-        },
+            },
+         },
       },
-    },
-  },
-  runtimeConfig: {
-    public: {
-      midjourney_api_key: process.env.NUXT_APP_MIDJOURNEY_API_KEY,
-      openai_api_key: process.env.NUXT_APP_OPENAI_API_KEY,
-      api_url: process.env.NUXT_APP_API_URL,
-    },
-  },
-  nitro: {
-    prerender: {
-      // crawlLinks: true,
-      // failOnError: false,
-    },
-  },
+   },
+   runtimeConfig: {
+      public: {
+         midjourney_api_key: process.env.NUXT_APP_MIDJOURNEY_API_KEY,
+         openai_api_key: process.env.NUXT_APP_OPENAI_API_KEY,
+         api_url: process.env.NUXT_APP_API_URL,
+      },
+   },
+   nitro: {
+      prerender: {
+         // crawlLinks: true,
+         // failOnError: false,
+      },
+   },
+   routeRules: {
+      '/terms': {
+         redirect: 'https://dvxgames.net/terms'
+      },
+      '/terms-to-use': {
+         redirect: 'https://dvxgames.net/terms'
+      },
+      // '/terms': {
+      //    redirect: 'https://dvxgames.net/terms'
+      // },
+      // '/terms': {
+      //    redirect: 'https://dvxgames.net/terms'
+      // },
+      // '/terms': {
+      //    redirect: 'https://dvxgames.net/terms'
+      // },
+      // '/terms': {
+      //    redirect: 'https://dvxgames.net/terms'
+      // },
+
+   }
 })
