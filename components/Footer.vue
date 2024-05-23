982888cecc3359cb4465c4f1511d4e686e6597f7
Alex W0lf <w0lfram@yandex.ru>
2024-05-23 17:31:06 +0300

    Header and Footer were prettierd

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,40 +1,52 @@
 <template>
-  <div>
-    <div class="wrap-container">
-      <footer class="footer">
-        <nav class="navigate">
-          <ul class="navigate__list">
-            <li><NuxtLink to="contact" class="navigate__link"> Contact </NuxtLink></li>
-              <li><NuxtLink to="privacy" class="navigate__link"> Privacy Policy </NuxtLink></li>
-              <li><NuxtLink to="imprint" class="navigate__link"> Imprint </NuxtLink></li>
-          </ul>
-        </nav>
-      </footer>
-    </div>
-  </div>
+	<div>
+		<div class="wrap-container">
+			<footer class="footer">
+				<nav class="navigate">
+					<ul class="navigate__list">
+						<li>
+							<NuxtLink to="contact" class="navigate__link">
+								Contact
+							</NuxtLink>
+						</li>
+						<li>
+							<NuxtLink to="privacy" class="navigate__link">
+								Privacy Policy
+							</NuxtLink>
+						</li>
+						<li>
+							<NuxtLink to="imprint" class="navigate__link">
+								Imprint
+							</NuxtLink>
+						</li>
+					</ul>
+				</nav>
+			</footer>
+		</div>
+	</div>
 </template>
 
 <script lang="ts" setup></script>
 
 <style lang="scss" scoped>
 .footer {
-  margin-top: 26px;
-  margin-bottom: 100px;
-  @media (max-width: 768px) {
-    margin-bottom: 45px;
-  }
+	margin-top: 26px;
+	margin-bottom: 100px;
+	@media (max-width: 768px) {
+		margin-bottom: 45px;
+	}
 }
 .navigate {
-  &__list {
-    display: flex;
-    justify-content: center;
-  }
-  &__link {
-    margin-left: 6px;
-    margin-right: 6px;
-    font-size: var(--text-6-font-size);
-    font-weight: 400;
-    color: var(--grey);
-  }
+	&__list {
+		display: flex;
+		justify-content: center;
+	}
+	&__link {
+		margin-left: 6px;
+		margin-right: 6px;
+		font-size: var(--text-6-font-size);
+		font-weight: 400;
+		color: var(--grey);
+	}
 }
 </style>
--- components/Header.vue
+++ components/Header.vue
@@ -1,186 +1,236 @@
 <template>
-  <div>
-    <div class="wrap-container">
-      <div class="header">
-        <div class="header__logo">
-          <NuxtLink to="/" class="header__logo-link">DVX.</NuxtLink>
-        </div>
-        <div class="header__navigate">
-          <nav class="navigate">
-            <ul class="navigate__list">
-              <li><NuxtLink to="contact" class="navigate__link"> Contact </NuxtLink></li>
-              <li><NuxtLink to="privacy" class="navigate__link"> Privacy Policy </NuxtLink></li>
-              <li><NuxtLink to="imprint" class="navigate__link"> Imprint </NuxtLink></li>
-            </ul>
-          </nav>
-        </div>
-        <div class="menu-burger" :status="modal_status == 'close' ? 'close' : 'open'">
-          <button @click="toggle_menu" class="menu-burger__btn-open">
-            <img class="menu-burger__btn-open-img" src="/img/burger.svg" alt="img: burger-menu-btn" />
-          </button>
-          <div class="menu-burger__container">
-            <div @click.self="toggle_menu" class="menu-burger__ground"></div>
-            <div class="menu-burger__body">
-              <button @click="toggle_menu" class="menu-burger__btn-close"><span class="cross-line"></span><span class="cross-line"></span></button>
-              <ul class="menu-burger__list">
-                <li>
-                  <NuxtLink @click="toggle_menu" to="/" class="menu-burger__link">Our apps</NuxtLink>
-                </li>
-                <li>
-                  <NuxtLink @click="toggle_menu" to="contact" class="menu-burger__link">Contact</NuxtLink>
-                </li>
-                <li>
-                  <NuxtLink @click="toggle_menu" to="privacy" class="menu-burger__link">Private Policy</NuxtLink>
-                </li>
-                <li>
-                  <NuxtLink @click="toggle_menu" to="imprint" class="menu-burger__link">Imprint</NuxtLink>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+	<div>
+		<div class="wrap-container">
+			<div class="header">
+				<div class="header__logo">
+					<NuxtLink to="/" class="header__logo-link">DVX.</NuxtLink>
+				</div>
+				<div class="header__navigate">
+					<nav class="navigate">
+						<ul class="navigate__list">
+							<li>
+								<NuxtLink to="contact" class="navigate__link">
+									Contact
+								</NuxtLink>
+							</li>
+							<li>
+								<NuxtLink to="privacy" class="navigate__link">
+									Privacy Policy
+								</NuxtLink>
+							</li>
+							<li>
+								<NuxtLink to="imprint" class="navigate__link">
+									Imprint
+								</NuxtLink>
+							</li>
+						</ul>
+					</nav>
+				</div>
+				<div
+					class="menu-burger"
+					:status="modal_status == 'close' ? 'close' : 'open'"
+				>
+					<button @click="toggle_menu" class="menu-burger__btn-open">
+						<img
+							class="menu-burger__btn-open-img"
+							src="/img/burger.svg"
+							alt="img: burger-menu-btn"
+						/>
+					</button>
+					<div class="menu-burger__container">
+						<div
+							@click.self="toggle_menu"
+							class="menu-burger__ground"
+						></div>
+						<div class="menu-burger__body">
+							<button
+								@click="toggle_menu"
+								class="menu-burger__btn-close"
+							>
+								<span class="cross-line"></span
+								><span class="cross-line"></span>
+							</button>
+							<ul class="menu-burger__list">
+								<li>
+									<NuxtLink
+										@click="toggle_menu"
+										to="/"
+										class="menu-burger__link"
+										>Our apps</NuxtLink
+									>
+								</li>
+								<li>
+									<NuxtLink
+										@click="toggle_menu"
+										to="contact"
+										class="menu-burger__link"
+										>Contact</NuxtLink
+									>
+								</li>
+								<li>
+									<NuxtLink
+										@click="toggle_menu"
+										to="privacy"
+										class="menu-burger__link"
+										>Private Policy</NuxtLink
+									>
+								</li>
+								<li>
+									<NuxtLink
+										@click="toggle_menu"
+										to="imprint"
+										class="menu-burger__link"
+										>Imprint</NuxtLink
+									>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script lang="ts" setup>
-const modal_status = ref('close');
+const modal_status = ref('close')
 
 function toggle_menu() {
-  if (modal_status.value === 'close') {
-    modal_status.value = 'open';
-  } else {
-    modal_status.value = 'close';
-  }
+	if (modal_status.value === 'close') {
+		modal_status.value = 'open'
+	} else {
+		modal_status.value = 'close'
+	}
 }
 </script>
 
 <style lang="scss" scoped>
 .menu-burger {
-  display: none;
-  @media screen and (max-width: 435px) {
-    display: block;
-  }
-  &__container {
-    position: fixed;
-    left: 0;
-    top: 0;
-    bottom: 0;
-    width: 200%;
-    z-index: 10;
-  }
-  &__btn-open {
-    cursor: pointer;
-  }
-  &__btn-open-img {
-    width: 40px;
-    height: 40px;
-  }
-  &__ground {
-    background-color: rgb(0 0 0 / 0.5);
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-  }
-  &__body {
-    width: 192px;
-    background-color: white;
-    height: 100vh;
-    padding: 48px 30px 30px;
-    position: relative;
-    z-index: 10;
-  }
-  &__btn-close {
-    cursor: pointer;
-    position: absolute;
-    top: 5px;
-    right: 5px;
-    height: 24px;
-    width: 24px;
-    padding: 10px;
-    box-sizing: content-box;
-    & .cross-line {
-      display: block;
-      height: 2px;
-      width: 24px;
-      background-color: black;
-      &:first-child {
-        transform: rotate(45deg) translateY(1px);
-      }
-      &:last-child {
-        transform: rotate(-45deg) translateY(-1px);
-      }
-    }
-  }
-  &__link {
-    padding: 9px 0;
-    display: block;
-    color: var(--grey);
-    font-size: var(--text-3-font-size);
-    font-weight: 600;
-    line-height: 1.5;
-    border-bottom: 1px solid var(--light-grey);
-  }
-  &[status='open'] {
-    .menu-burger__container {
-      transform: translateX(0);
-    }
-    .menu-burger__ground {
-      opacity: 1;
-      z-index: 5;
-      transition: 0.3s opacity ease-in-out;
-    }
-    .menu-burger__body {
-      transform: translateX(0);
-      transition: 0.4s transform ease-in-out;
-    }
-  }
-  &[status='close'] {
-    .menu-burger__container {
-      transform: translateX(-100%);
-      transition: 0.75s transform ease-in-out;
-    }
-    .menu-burger__ground {
-      opacity: 0;
-      z-index: -5;
-      transition: 0.3s opacity, z-index ease-in-out;
-    }
-    .menu-burger__body {
-      transform: translateX(-100%);
-      transition: 0.75s transform ease-in-out;
-    }
-  }
+	display: none;
+	@media screen and (max-width: 435px) {
+		display: block;
+	}
+	&__container {
+		position: fixed;
+		left: 0;
+		top: 0;
+		bottom: 0;
+		width: 200%;
+		z-index: 10;
+	}
+	&__btn-open {
+		cursor: pointer;
+	}
+	&__btn-open-img {
+		width: 40px;
+		height: 40px;
+	}
+	&__ground {
+		background-color: rgb(0 0 0 / 0.5);
+		position: absolute;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+	}
+	&__body {
+		width: 192px;
+		background-color: white;
+		height: 100vh;
+		padding: 48px 30px 30px;
+		position: relative;
+		z-index: 10;
+	}
+	&__btn-close {
+		cursor: pointer;
+		position: absolute;
+		top: 5px;
+		right: 5px;
+		height: 24px;
+		width: 24px;
+		padding: 10px;
+		box-sizing: content-box;
+		& .cross-line {
+			display: block;
+			height: 2px;
+			width: 24px;
+			background-color: black;
+			&:first-child {
+				transform: rotate(45deg) translateY(1px);
+			}
+			&:last-child {
+				transform: rotate(-45deg) translateY(-1px);
+			}
+		}
+	}
+	&__link {
+		padding: 9px 0;
+		display: block;
+		color: var(--grey);
+		font-size: var(--text-3-font-size);
+		font-weight: 600;
+		line-height: 1.5;
+		border-bottom: 1px solid var(--light-grey);
+	}
+	&[status='open'] {
+		.menu-burger__container {
+			transform: translateX(0);
+		}
+		.menu-burger__ground {
+			opacity: 1;
+			z-index: 5;
+			transition: 0.3s opacity ease-in-out;
+		}
+		.menu-burger__body {
+			transform: translateX(0);
+			transition: 0.4s transform ease-in-out;
+		}
+	}
+	&[status='close'] {
+		.menu-burger__container {
+			transform: translateX(-100%);
+			transition: 0.75s transform ease-in-out;
+		}
+		.menu-burger__ground {
+			opacity: 0;
+			z-index: -5;
+			transition:
+				0.3s opacity,
+				z-index ease-in-out;
+		}
+		.menu-burger__body {
+			transform: translateX(-100%);
+			transition: 0.75s transform ease-in-out;
+		}
+	}
 }
 .header {
-  padding: 15px 0 15px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  &__logo-link {
-    font: 400 40px 'Karantina';
-    color: var(--grey);
-  }
-  &__navigate {
-    @media screen and (max-width: 435px) {
-      display: none;
-    }
-  }
+	padding: 15px 0 15px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	&__logo-link {
+		font: 400 40px 'Karantina';
+		color: var(--grey);
+	}
+	&__navigate {
+		@media screen and (max-width: 435px) {
+			display: none;
+		}
+	}
 }
 .navigate {
-  &__list {
-    display: flex;
-  }
-  &__link {
-    margin-left: 20px;
-    font-size: var(--text-5-font-size);
-    color: var(--grey);
-  }
+	&__list {
+		display: flex;
+	}
+	&__link {
+		margin-left: 20px;
+		font-size: var(--text-5-font-size);
+		color: var(--grey);
+	}
 }
 
 .router-link-exact-active {
-  color: var(--blue);
+	color: var(--blue);
 }
 </style>
