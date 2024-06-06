46305903e55124e741ffc758cd99d3cbb1bc3c46
devilnaz <ermolilnaz@ya.ru>
2024-06-06 15:35:00 +0300

    change color checkbox

--- pages/contact-form.vue
+++ pages/contact-form.vue
@@ -288,17 +288,17 @@ async function send_email(e) {
 		transition: all 0.2s ease-out;
 	}
 	&:checked::before {
-		border-color: #0b76ef;
-		background-color: #0b76ef;
+		border-color: var(--blue);
+		background-color: var(--blue);
 		background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8'%3e%3cpath fill='%23fff' d='M6.564.75l-3.59 3.612-1.538-1.55L0 4.26 2.974 7.25 8 2.193z'/%3e%3c/svg%3e");
 		transition: all 0.2s ease-out;
 	}
 	&:focus::before {
-		box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
+		box-shadow: 0 0 0 0.2rem rgba(5, 127, 220, 0.25);
 		transition: all 0.2s ease-out;
 	}
 	&:focus:not(:checked)::before {
-		border-color: #80bdff;
+		border-color: var(--blue);
 		transition: all 0.2s ease-out;
 	}
 }
