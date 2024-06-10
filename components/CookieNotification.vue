31012702ca40f9b4b8a178c6d734c3a624f3c89e
devilnaz <ermolilnaz@ya.ru>
2024-06-10 16:55:02 +0300

    change cookie notification

--- components/CookieNotification.vue
+++ components/CookieNotification.vue
@@ -1,10 +1,16 @@
 <template>
-	<div v-if="visible_notification" class="notification-cookie">
+	<div
+		v-if="is_visible"
+		ref="element_notification"
+		class="notification-cookie"
+	>
 		<p class="notification-cookie__desc">
 			Diese Website verwendet Cookies, um die Nutzerfreundlichkeit zu
 			verbessern. Durch die weitere Nutzung der Website stimmen Sie dem zu.
 			Weitere Infos zu Cookies und deren Deaktivierung finden Sie
-			<a class="notification-cookie__link" href="#">hier</a>.
+			<a class="notification-cookie__link" @click="close_notification"
+				>hier</a
+			>.
 		</p>
 		<button
 			@click="close_notification"
@@ -20,7 +26,8 @@
 </template>
 
 <script lang="ts" setup>
-let visible_notification = ref(false)
+let is_visible = ref(false)
+const element_notification = ref(null)
 
 const current_date = new Date()
 const get_new_date = new Date(
@@ -31,21 +38,33 @@ const show_notif_cookie = useCookie('show-notif-cookie', {
 	expires: get_new_date,
 })
 
+if (typeof show_notif_cookie.value === 'undefined') {
+	show_notif_cookie.value = 'true'
+}
+is_visible.value = JSON.parse(show_notif_cookie.value)
+
 onMounted(() => {
-	if (typeof show_notif_cookie.value === 'undefined') {
-		show_notif_cookie.value = 'true'
+	if (is_visible.value === true) {
+		setTimeout(() => {
+			element_notification.value.classList.remove('notification-hide')
+			element_notification.value.classList.add('notification-show')
+		}, 500)
 	}
-	visible_notification.value = JSON.parse(show_notif_cookie.value)
 })
 
 function close_notification() {
+	element_notification.value.classList.remove('notification-show')
+	element_notification.value.classList.add('notification-hide')
 	show_notif_cookie.value = 'false'
-	visible_notification.value = false
+	setTimeout(() => {
+		is_visible.value = false
+	}, 1000)
 }
 </script>
 
 <style lang="scss">
 .notification-cookie {
+	transform: translateY(500%);
 	position: fixed;
 	bottom: 0;
 	left: 0;
@@ -61,6 +80,9 @@ function close_notification() {
 	@media (max-width: 486px) {
 		padding-left: 20px;
 	}
+	&__desc {
+		text-align: center;
+	}
 	&__desc,
 	&__link {
 		font-family: 'Gotham XNarrow';
@@ -70,6 +92,13 @@ function close_notification() {
 	}
 	&__link {
 		text-decoration: underline;
+		&:hover {
+			cursor: pointer;
+		}
+		&:hover,
+		&:active {
+			color: #fff;
+		}
 	}
 	&__btn-close {
 		border-radius: 50%;
@@ -90,6 +119,35 @@ function close_notification() {
 	&__btn-close-img {
 		display: block;
 		width: 100%;
+	}
+}
+
+.notification-show {
+	animation: slide-top 1s forwards;
+}
+
+.notification-hide {
+	animation: slide-bottom 1s forwards;
+}
+
+@keyframes slide-top {
+	0% {
+		transform: translateY(500%);
+	}
+	50% {
+		transform: translateY(300%);
+	}
+	to {
+		transform: translateY(0%);
+	}
+}
+
+@keyframes slide-bottom {
+	0% {
+		transform: translateY(0%);
+	}
+	to {
+		transform: translateY(500%);
 	}
 }
 </style>
